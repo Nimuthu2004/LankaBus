ed029e140aecf743daeb988c599206334cb0a55c
nisuradulwan <nisuradulwan@gmail.com>
2026-02-23 17:23:44 +0530

    imported packaged to loginTypeScrenn.dart

--- frontend/lib/Screens/login_type_screen.dart
+++ frontend/lib/Screens/login_type_screen.dart
@@ -1,1 +1,5 @@
-
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart';
+import 'login_screen.dart';
+import 'conductor_register_screen.dart';
+import 'splash_screen.dart';
