bae99c2015597faa6885e991cc48c8f021fb04f7
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-02-23 17:02:36 +0530

    created class ConductorRegisterScreen

--- frontend/lib/Screens/conductor_register_screen.dart
+++ frontend/lib/Screens/conductor_register_screen.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_button.dart';
 import '../widgets/custom_textfield.dart';
+
+class ConductorRegisterScreen extends StatelessWidget {
+  const ConductorRegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: gradientBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+            children: const [
+              CustomTextField(hint: "Bus Registration Number"),
+              CustomTextField(hint: "Company Name"),
+              CustomTextField(hint: "Route"),
+              CustomTextField(hint: "NTC Registration Number"),
+              CustomTextField(hint: "Conductor Full Name"),
+              CustomTextField(hint: "Driver Full Name"),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: CustomButton(
+        text: "Submit",
+        onPressed: () {
+          // TODO: Implement the submit functionality
+          print("Submit button pressed");
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
