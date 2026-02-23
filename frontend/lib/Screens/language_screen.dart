1a500c80449a7391b385ada7efe234c71a2cf1c2
Binukav <vithanagebinuka@gmail.com>
2026-02-23 17:35:45 +0530

    created class LanguageScreen extended StatelessWidget

--- frontend/lib/Screens/language_screen.dart
+++ frontend/lib/Screens/language_screen.dart
@@ -1,3 +1,47 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_button.dart';
 import 'login_type_screen.dart';
+
+class LanguageScreen extends StatelessWidget {
+  const LanguageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: gradientBackground(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Select Preferred Language:",
+                  style: TextStyle(fontSize: 22, color: Colors.white)),
+              const SizedBox(height: 30),
+              CustomButton(
+                  text: "English",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const LoginTypeScreen()),
+                    );
+                  }),
+              const SizedBox(height: 15),
+              CustomButton(
+                  text: "සිංහල",
+                  onPressed: () {
+                    // TODO: Implement navigation or functionality for Sinhala
+                    print("Sinhala button pressed");
+                  }),
+              const SizedBox(height: 15),
+              CustomButton(
+                  text: "தமிழ்",
+                  onPressed: () {
+                    // TODO: Implement navigation or functionality for Tamil
+                    print("Tamil button pressed");
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
