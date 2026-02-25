49e9855f7760b85734343b6c96f4ddeaf6a6b228
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-02-25 20:15:14 +0530

    add gradientBackground helper function

--- frontend/lib/Screens/conductor_register_screen.dart
+++ frontend/lib/Screens/conductor_register_screen.dart
@@ -35,3 +35,16 @@ class ConductorRegisterScreen extends StatelessWidget {
     );
   }
 }
+  // Placeholder for the gradientBackground function
+  Widget gradientBackground({required Widget child}) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blue, Colors.purple],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: child,
+    );
+  }
