66056c0e438c1ba5e1a8c9bca90eb347339f55f9
Binukav <vithanagebinuka@gmail.com>
2026-02-25 21:49:41 +0530

    Create TravelBookingSlide stateless widget structure

---
+++ frontend/lib/Screens/destination select screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class TravelBookingSlide extends StatelessWidget {
+  const TravelBookingSlide({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          width: 400,
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 5,
+                blurRadius: 15,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
