8b91321ce51ab2f176038563246168da4cecb53f
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-02-25 20:41:15 +0530

    create HomeScreen StatefulWidget

--- frontend/lib/Screens/home_screen.dart
+++ frontend/lib/Screens/home_screen.dart
@@ -1,3 +1,13 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'qr_scan_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
