cd50184bd2094195d5e7e3251f680fc6c3eb973f
Binukav <vithanagebinuka@gmail.com>
2026-03-13 14:44:13 +0530

    Add required screen and Flutter imports

---
+++ frontend/lankabus/lib/conductor home screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'scan_qr_screen.dart';
+import 'seat_update_screen.dart';
+import 'summary_screen.dart';
+
+import 'conductor_chatbot_screen.dart';
+import 'conductor_settings_screen.dart';
+import 'conductor_profile_screen.dart';
