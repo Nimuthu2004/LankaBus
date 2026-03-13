4e3e352b405f7635578037bc02bb77502f77b209
Binukav <vithanagebinuka@gmail.com>
2026-03-14 02:26:52 +0530

    Create conductor login screen layout

--- frontend/lankabus/lib/conductor login screen.dart
+++ frontend/lankabus/lib/conductor login screen.dart
@@ -4,3 +4,38 @@ import '../widgets/custom_textfield.dart';
 import 'conductor_home_screen.dart';
 import 'conductor_register_screen.dart';
 import 'forgot_password_screen.dart';
+class ConductorLoginScreen extends StatelessWidget {
+  const ConductorLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF1E3C72), Color(0xFF2A5298)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Conductor Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+
+                  const CustomTextField(hint: "Email or Username"),
+                  const CustomTextField(hint: "Password", obscure: true),
+                  const SizedBox(height: 10),
+                  
