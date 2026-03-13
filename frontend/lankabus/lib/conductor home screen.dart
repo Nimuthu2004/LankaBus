93481e274ca276f5bed9b3a4f84e46043fbcda66
Binukav <vithanagebinuka@gmail.com>
2026-03-13 15:00:48 +0530

    Create ConductorHomeScreen stateful widget

--- frontend/lankabus/lib/conductor home screen.dart
+++ frontend/lankabus/lib/conductor home screen.dart
@@ -6,3 +6,19 @@ import 'summary_screen.dart';
 import 'conductor_chatbot_screen.dart';
 import 'conductor_settings_screen.dart';
 import 'conductor_profile_screen.dart';
+class ConductorHomeScreen extends StatefulWidget {
+  const ConductorHomeScreen({super.key});
+
+  @override
+  State<ConductorHomeScreen> createState() => _ConductorHomeScreenState();
+}
+
+class _ConductorHomeScreenState extends State<ConductorHomeScreen> {
+
+  int passengerCount = 0;
+
+  void increasePassenger() {
+    setState(() {
+      passengerCount++;
+    });
+  }
