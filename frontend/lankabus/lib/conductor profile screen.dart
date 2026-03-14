ce4da735b3d38bf1afaff7be5b4c5d8ca6114203
Binukav <vithanagebinuka@gmail.com>
2026-03-14 22:51:02 +0530

    Create ConductorProfileScreen structure

---
+++ frontend/lankabus/lib/conductor profile screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ConductorProfileScreen extends StatelessWidget {
+  const ConductorProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Conductor Profile"),
+      ),
