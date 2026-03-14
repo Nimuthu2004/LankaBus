31f872b3dd92a634deaaf011fa1b729e7fd906dd
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-14 11:56:45 +0530

    create base structure for CustomButton widget

---
+++ frontend/lankabus/lib/widgets/custom_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
