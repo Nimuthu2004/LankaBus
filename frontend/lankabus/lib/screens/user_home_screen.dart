df39a35c4995bb5f4cec3fcb5930e24fb52da450
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-15 21:28:46 +0530

    Initialize UserHomeScreen with required screen imports

---
+++ frontend/lankabus/lib/screens/user_home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'ticket_screen.dart';
+import 'route_screen.dart';
+import 'wallet_screen.dart';
+import 'history_screen.dart';
+import 'settings_screen.dart';
+import 'profile_screen.dart';
+import 'chatbot_screen.dart';
+
+class UserHomeScreen extends StatelessWidget {
+  const UserHomeScreen({super.key});
