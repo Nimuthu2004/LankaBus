3ef8ea21edcbaa9cafb97be89026345af784914a
nisuradulwan <nisuradulwan@gmail.com>
2026-03-15 11:15:25 +0530

    create  language  screen dart file and add language screen structure and screen import

---
+++ frontend/lankabus/lib/screens/language_screen.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'login_type_screen.dart';
+
+class LanguageScreen extends StatelessWidget {
+  const LanguageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {}
+}
