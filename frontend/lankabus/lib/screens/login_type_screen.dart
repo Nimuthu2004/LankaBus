0ba51524fb7ced1c2b4e839407fc1f8674cd68d3
nisuradulwan <nisuradulwan@gmail.com>
2026-03-16 23:18:56 +0530

    add login type screen layout and branding section

--- frontend/lankabus/lib/screens/login_type_screen.dart
+++ frontend/lankabus/lib/screens/login_type_screen.dart
@@ -7,5 +7,49 @@ class LoginTypeScreen extends StatelessWidget {
   const LoginTypeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {}
-}
+  Widget build(BuildContext context) {
+      return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF1E3C72), Color(0xFF2A5298)],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Logo Image
+              Image.asset(
+                'assets/images/logo.png',
+                width: 120,
+                height: 120,
+              ),
+              const SizedBox(height: 20),
+
+              // App Title
+              const Text(
+                "Welcome to Lanka Bus",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 50),
+  
+          
+             )
+          )
+        )
+      )
+  
+     
+
+    
+   }
+  }
+
