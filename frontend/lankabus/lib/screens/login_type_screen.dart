674339b1c60152833e2c7c277d97d1456385cf5c
nisuradulwan <nisuradulwan@gmail.com>
2026-03-16 23:10:59 +0530

    add LoginTypeScreen structure and dependencies

--- frontend/lankabus/lib/screens/login_type_screen.dart
+++ frontend/lankabus/lib/screens/login_type_screen.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart';
+import 'user_login_screen.dart';
+import 'conductor_login_screen.dart';
 
+class LoginTypeScreen extends StatelessWidget {
+  const LoginTypeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {}
+}
