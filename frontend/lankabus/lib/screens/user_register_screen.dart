22e536d7c22f7f11299bf9fc0c5894066fd62909
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-17 11:48:24 +0530

    Initialize user register screen structure and state variables

---
+++ frontend/lankabus/lib/screens/user_register_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class UserRegisterScreen extends StatefulWidget {
+  const UserRegisterScreen({super.key});
+
+  @override
+  State<UserRegisterScreen> createState() => _UserRegisterScreenState();
+}
+
+class _UserRegisterScreenState extends State<UserRegisterScreen> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  bool agreeTerms = false;
+  bool obscurePassword = true;
+  bool obscureConfirmPassword = true;
