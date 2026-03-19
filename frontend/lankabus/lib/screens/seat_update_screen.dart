98bf36c6a41cad03a1dc18879ac22cce35dc685e
nisuradulwan <nisuradulwan@gmail.com>
2026-03-19 11:08:36 +0530

    add seat state tracking and seat tile builder

--- frontend/lankabus/lib/screens/seat_update_screen.dart
+++ frontend/lankabus/lib/screens/seat_update_screen.dart
@@ -7,4 +7,33 @@ class SeatUpdateScreen extends StatefulWidget {
   State<SeatUpdateScreen> createState() => _SeatUpdateScreenState();
 }
 
-class _SeatUpdateScreenState extends State<SeatUpdateScreen> {}
+class _SeatUpdateScreenState extends State<SeatUpdateScreen> {
+  List<bool> seats = List.generate(20, (index) => false);
+
+  Widget buildSeat(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          seats[index] = !seats[index];
+        });
+      },
+      child: Container(
+        height: 55,
+        width: 55,
+        decoration: BoxDecoration(
+          color: seats[index] ? Colors.red : Colors.green,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            "S${index + 1}",
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
