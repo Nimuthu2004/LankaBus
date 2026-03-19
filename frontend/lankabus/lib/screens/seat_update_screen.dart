33b06ad6565e2a6ff8fa6665f25ae87ef1305978
nisuradulwan <nisuradulwan@gmail.com>
2026-03-19 11:06:57 +0530

    add SeatUpdateScreen stateful widget

---
+++ frontend/lankabus/lib/screens/seat_update_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeatUpdateScreen extends StatefulWidget {
+  const SeatUpdateScreen({super.key});
+
+  @override
+  State<SeatUpdateScreen> createState() => _SeatUpdateScreenState();
+}
+
+class _SeatUpdateScreenState extends State<SeatUpdateScreen> {}
