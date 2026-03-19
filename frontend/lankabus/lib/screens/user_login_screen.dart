44d9b61409d944b23d89d5438ed69d77f202feb4
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-19 09:23:43 +0530

    Add user login screen UI layout and input fields

---
+++ frontend/lankabus/lib/screens/user_login_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/custom_textfield.dart';
+import 'user_home_screen.dart';
+import 'user_register_screen.dart';
+import 'forgot_password_screen.dart';
+
+class UserLoginScreen extends StatelessWidget {
+  const UserLoginScreen({super.key});
