c0a72d93d4221f8223c3bf3cc3b7e39fdad507a6
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-20 22:02:59 +0530

    Create route screen

---
+++ frontend/lankabus/lib/screens/route_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RouteScreen extends StatelessWidget {
+  const RouteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Route"),
+      ),
+      body: const Center(
+        child: Text("Route Screen"),
+      ),
+    );
+  }
+}
