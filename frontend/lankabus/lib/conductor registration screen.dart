25c027a2ce1776178a8f01a546b962113e0ce1ba
Binukav <vithanagebinuka@gmail.com>
2026-03-20 14:05:49 +0530

    Initialize ConductorRegisterScreen stateful widget

--- frontend/lankabus/lib/conductor registration screen.dart
+++ frontend/lankabus/lib/conductor registration screen.dart
@@ -2,3 +2,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../widgets/custom_button.dart';
+class ConductorRegisterScreen extends StatefulWidget {
+  const ConductorRegisterScreen({super.key});
+
+  @override
+  State<ConductorRegisterScreen> createState() =>
+      _ConductorRegisterScreenState();
+}
