513f55a6066768e2619529bf7f392eeb0de97733
nisuradulwan <nisuradulwan@gmail.com>
2026-03-20 15:42:53 +0530

    add profile screen structure and app bar

---
+++ frontend/lankabus/lib/screens/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+        backgroundColor: const Color(0xFF1E3C72),
+      ),
+  }
+  }
