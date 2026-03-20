befaca5455696830bebd175365f9e57ad19838b8
nisuradulwan <nisuradulwan@gmail.com>
2026-03-20 16:02:17 +0530

    fix  same code again write issue

--- frontend/lankabus/lib/screens/profile_screen.dart
+++ frontend/lankabus/lib/screens/profile_screen.dart
@@ -33,24 +33,6 @@ class ProfileScreen extends StatelessWidget {
 
           const SizedBox(height: 30),
 
-          const CircleAvatar(
-            radius: 60,
-            backgroundImage: AssetImage("assets/images/profile.jpg"),
-          ),
-
-          const SizedBox(height: 20),
-
-          const Text(
-            "John Doe",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-
-          const SizedBox(height: 5),
-
-          const Text("john@email.com", style: TextStyle(color: Colors.grey)),
-
-          const SizedBox(height: 30),
-
           ListTile(
             leading: const Icon(Icons.person),
             title: const Text("Edit Profile"),
