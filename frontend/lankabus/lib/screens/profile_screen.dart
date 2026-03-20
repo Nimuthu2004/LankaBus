515201d651805301d9277e86b6489d5079b2f02e
nisuradulwan <nisuradulwan@gmail.com>
2026-03-20 15:47:12 +0530

    add profile header with avatar and user info

--- frontend/lankabus/lib/screens/profile_screen.dart
+++ frontend/lankabus/lib/screens/profile_screen.dart
@@ -10,5 +10,41 @@ class ProfileScreen extends StatelessWidget {
         title: const Text("Profile"),
         backgroundColor: const Color(0xFF1E3C72),
       ),
+  
+  body: Column(
+        children: [
+
+          const SizedBox(height: 30),
+
+          const CircleAvatar(
+            radius: 60,
+            backgroundImage: AssetImage("assets/images/profile.jpg"),
+          ),
+
+          const SizedBox(height: 20),
+
+          const Text(
+            "John Doe",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          const SizedBox(height: 5),
+
+          const Text(
+            "john@email.com",
+            style: TextStyle(color: Colors.grey),
+          ),
+
+          const SizedBox(height: 30),
+  
+  
+  
+  
+        ]
+  )
+  )
   }
   }
