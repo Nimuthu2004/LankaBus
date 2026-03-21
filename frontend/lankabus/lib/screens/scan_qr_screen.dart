5962f26627f94a3ac4b1ad9c702493fff820aa73
nisuradulwan <nisuradulwan@gmail.com>
2026-03-21 22:04:50 +0530

    add ScanQRScreen structure and app bar

---
+++ frontend/lankabus/lib/screens/scan_qr_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ScanQRScreen extends StatelessWidget {
+  const ScanQRScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Scan QR Code"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+     )
+   }
+  }
+  
