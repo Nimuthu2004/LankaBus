a1fae2243d05d647418711c5d90af49d7765f312
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-21 14:21:12 +0530

    Implement splash screen animations and navigation logic

--- frontend/lankabus/lib/screens/splash_screen.dart
+++ frontend/lankabus/lib/screens/splash_screen.dart
@@ -7,3 +7,39 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _progressAnimation;
+  late Animation<double> _shimmerAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _fadeAnimation =
+        CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _progressAnimation =
+        CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+
+    _shimmerAnimation =
+        Tween<double>(begin: -1, end: 2).animate(_controller);
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LanguageScreen()),
+      );
+    });
+  }
