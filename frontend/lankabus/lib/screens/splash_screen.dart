fffa20281a360717fe07e16858a760639b68c581
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-21 14:20:17 +0530

    Created splash screen and initialize splash screen widget structure

---
+++ frontend/lankabus/lib/screens/splash_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'language_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
