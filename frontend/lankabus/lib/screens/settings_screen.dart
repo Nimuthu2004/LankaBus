7581b5f6c355e18567b661880540f6c79dab7650
nisuradulwan <nisuradulwan@gmail.com>
2026-03-21 21:36:30 +0530

    create setting_screen.dart file and create settings screen layout with scaffold and app bar

---
+++ frontend/lankabus/lib/screens/settings_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        backgroundColor: const Color(0xFF1E3C72),
+      ),
+      body: ListView(
+        children: const [
+         
+
+  
+        ]
+      )
+    )   
+   }
+  }
