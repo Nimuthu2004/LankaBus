1e439282ce4edb6df2163666d0d04d9f3c71780a
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-22 12:19:55 +0530

    create ForgotPasswordScreen stateful widget

--- frontend/lankabus/lib/screens/forgot_password_screen.dart
+++ frontend/lankabus/lib/screens/forgot_password_screen.dart
@@ -4,3 +4,10 @@ import '../widgets/custom_button.dart';
 import '../widgets/custom_textfield.dart';
 
 enum ResetStep { enterEmail, enterOTP, resetPassword }
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
