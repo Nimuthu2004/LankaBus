ffef7e281334e31ec1567af3064c28664bad8663
sasiduanjula242-sys <sasiduanjula242@gmail.com>
2026-03-22 12:19:22 +0530

    define reset password step enum

--- frontend/lankabus/lib/screens/forgot_password_screen.dart
+++ frontend/lankabus/lib/screens/forgot_password_screen.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../widgets/custom_button.dart';
 import '../widgets/custom_textfield.dart';
+
+enum ResetStep { enterEmail, enterOTP, resetPassword }
