44d836aa935da8dd0c2d18adc99b632bf13127b6
sonaldilmith2004 <sonaldilmith2004@gmail.com>
2026-03-23 11:05:43 +0530

    Update conductor login screen.dart

--- frontend/lankabus/lib/conductor login screen.dart
+++ frontend/lankabus/lib/conductor login screen.dart
@@ -14,7 +14,7 @@ class ConductorLoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF3455EB), Color(0xFF14387A)],
+            colors: [Color(0xFF1E3C72), Color(0xFF2A5298)],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
